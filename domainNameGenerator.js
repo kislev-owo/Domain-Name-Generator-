754dab33b366b5a5a4a806fcedbfa2938b049c6e
kislev-owo <kislev_083@hotmail.com>
2020-07-17 20:20:14 +0000

    ejercicio completoo

---
+++ domainNameGenerator.js
@@ -0,0 +1,16 @@
+
+    let pronoun = ['the','our'];
+    let adj = ['great', 'big'];
+    let noun = ['jogger','racoon'];
+    let domain = [ '.com', '.net'];
+    for (let pronombre of pronoun){
+    for (let adjetivo of adj){
+    for (let nombre of noun){
+    for (let dom of domain){
+    console.log(pronombre+adjetivo+nombre+dom);
+    }
+    }
+    } 
+    }
+    
+
