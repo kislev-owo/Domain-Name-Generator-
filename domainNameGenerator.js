719577c6e3a8b4492567f4a225fb737bd1470819
kislev-owo <kislev_083@hotmail.com>
2020-07-17 21:07:46 +0000

    actualizacion

--- domainNameGenerator.js
+++ domainNameGenerator.js
@@ -4,13 +4,13 @@
     let noun = ['jogger','racoon'];
     let domain = [ '.com', '.net'];
     for (let pronombre of pronoun){
-    for (let adjetivo of adj){
-    for (let nombre of noun){
-    for (let dom of domain){
-    console.log(pronombre+adjetivo+nombre+dom);
-    }
-    }
-    } 
+        for (let adjetivo of adj){
+            for (let nombre of noun){
+                for (let dom of domain){
+                    console.log(pronombre+adjetivo+nombre+dom);
+                }
+            }
+        } 
     }
     
 
